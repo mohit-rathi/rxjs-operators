e05fc7721877b14a2ee3ad0429d7e6d69b9ef8b1
mohit-rathi <mohit.rathi@bacancy.com>
2023-06-06 17:48:07 +0530

    Complete tap operator example

--- src/app/operator/tap/tap.component.ts
+++ src/app/operator/tap/tap.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-tap',
   templateUrl: './tap.component.html',
-  styleUrls: ['./tap.component.scss']
+  styleUrls: ['./tap.component.scss'],
 })
-export class TapComponent {
+export class TapComponent implements OnInit {
+  public tapArr: number[] = [];
+  public subArr: number[] = [];
 
+  ngOnInit(): void {
+    const obs1$ = of([1, 2, 3, 4, 5]);
+
+    obs1$
+      .pipe(
+        tap((data) => {
+          this.tapArr = data;
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          this.subArr = data;
+        },
+      });
+  }
 }
