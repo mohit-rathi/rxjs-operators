caa80bbfbb2c5609fc3beab1ab08f00725be003d
mohit-rathi <mohit.rathi@bacancy.com>
2023-06-11 10:52:40 +0530

    Add firebase config in env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyAhh6ZFLgroCXf5uqcBOgqB4kWMbIxLdxM',
+    authDomain: 'rxjs-8eb50.firebaseapp.com',
+    projectId: 'rxjs-8eb50',
+    storageBucket: 'rxjs-8eb50.appspot.com',
+    messagingSenderId: '222406534191',
+    appId: '1:222406534191:web:5b85e6d3a6531e2d5e92da',
+  },
+};
