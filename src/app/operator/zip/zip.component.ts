617ad8ff745da2b6f643edb21c977e2a4b53842b
mohit-rathi <mohit.rathi@bacancy.com>
2023-06-12 10:18:29 +0530

    Modify and add example for zip operator

--- src/app/operator/zip/zip.component.ts
+++ src/app/operator/zip/zip.component.ts
@@ -1,43 +1,68 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { fromEvent, map, zip } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription, interval, of, take, zip } from 'rxjs';
 
 @Component({
   selector: 'app-zip',
   templateUrl: './zip.component.html',
   styleUrls: ['./zip.component.scss'],
 })
-export class ZipComponent implements AfterViewInit {
-  @ViewChild('name') name!: ElementRef;
-  @ViewChild('color') color!: ElementRef;
+export class ZipComponent implements OnDestroy {
+  // Example - 01
+  private obs1$ = of(1, 2, 3, 4, 5);
+  private obs2$ = of('John', 'Bruce', 'Peter', 'Tony', 'Sam');
+  private zip1$ = zip(this.obs1$, this.obs2$);
+  private subscriptionOne!: Subscription;
+  public arrOne: [number, string][] = [];
 
-  public nameList: string[] = ['John', 'Bruce', 'Tony', 'Sam', 'Peter'];
-  public colorList: string[] = ['blue', 'yellow', 'pink', 'green', 'orange'];
+  // Example - 02
+  private obs3$ = interval(1000).pipe(take(5));
+  private obs4$ = interval(2000).pipe(take(5));
+  private zip2$ = zip(this.obs3$, this.obs4$);
+  private subscriptionTwo!: Subscription;
+  public arrTwo: [number, number][] = [];
 
-  public boxList: { content: string; class: string }[] = [];
-
-  ngAfterViewInit(): void {
-    const nameObs = fromEvent<any>(this.name.nativeElement, 'change').pipe(
-      map((event) => event.target.value)
-    );
-
-    const colorObs = fromEvent<any>(this.color.nativeElement, 'change').pipe(
-      map((event) => event.target.value)
-    );
-
-    // Example - 01 | Zip
-    zip(nameObs, colorObs).subscribe({
-      next: ([name, color]) => {
-        // console.log(name, color);
-        // this.createBox(name, color, 'elContainer');
-        this.boxList.push({ content: name, class: color });
+  // Example - 01
+  public subscribeOne(): void {
+    this.unsubscribeOne();
+    this.subscriptionOne = this.zip1$.subscribe({
+      next: (res) => {
+        this.arrOne.push(res);
       },
     });
   }
 
-  // public createBox(name: string, color: string, containerId: string) {
-  //   const el = document.createElement('div');
-  //   el.innerText = name;
-  //   el.setAttribute('class', color);
-  //   document.getElementById(containerId)?.appendChild(el);
-  // }
+  // Example - 01
+  public unsubscribeOne(): void {
+    if (this.subscriptionOne) {
+      this.arrOne.splice(0, this.arrOne.length);
+      this.subscriptionOne.unsubscribe();
+    }
+  }
+
+  // Example - 02
+  public subscribeTwo(): void {
+    this.unsubscribeTwo();
+    this.subscriptionTwo = this.zip2$.subscribe({
+      next: (res) => {
+        this.arrTwo.push(res);
+      },
+    });
+  }
+
+  // Example - 02
+  public unsubscribeTwo(): void {
+    if (this.subscriptionTwo) {
+      this.arrTwo.splice(0, this.arrTwo.length);
+      this.subscriptionTwo.unsubscribe();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscriptionOne) {
+      this.subscriptionOne.unsubscribe();
+    }
+    if (this.subscriptionTwo) {
+      this.subscriptionTwo.unsubscribe();
+    }
+  }
 }
