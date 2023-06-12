ccc7cab33dd8d1dd97ff1bdbffbd36d422d2f609
mohit-rathi <mohit.rathi@bacancy.com>
2023-06-12 16:44:45 +0530

    Modify merge component logic and template

--- src/app/operator/merge/merge.component.ts
+++ src/app/operator/merge/merge.component.ts
@@ -1,24 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, merge, take } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription, interval, merge, take } from 'rxjs';
 
 @Component({
   selector: 'app-merge',
   templateUrl: './merge.component.html',
   styleUrls: ['./merge.component.scss'],
 })
-export class MergeComponent implements OnInit {
-  public arr: number[] = [];
+export class MergeComponent implements OnDestroy {
+  // Example - 01
+  private obs1$ = interval(1000).pipe(take(5));
+  private obs2$ = interval(1500).pipe(take(5));
+  private mrg1$ = merge(this.obs1$, this.obs2$);
+  private subscriptionOne!: Subscription;
+  public arrOne: number[] = [];
 
-  ngOnInit(): void {
-    const obs1$ = interval(1000).pipe(take(3));
-    const obs2$ = interval(1500).pipe(take(3));
+  ngOnDestroy(): void {
+    if (this.subscriptionOne) {
+      this.subscriptionOne.unsubscribe();
+    }
+  }
 
-    const merge$ = merge(obs1$, obs2$);
-
-    merge$.subscribe({
-      next: (data) => {
-        this.arr.push(data);
+  // Example - 01
+  public subscribeOne(): void {
+    this.unsubscribeOne();
+    this.subscriptionOne = this.mrg1$.subscribe({
+      next: (res) => {
+        this.arrOne.push(res);
       },
     });
+  }
+
+  // Example - 01
+  public unsubscribeOne(): void {
+    if (this.subscriptionOne) {
+      this.arrOne.splice(0, this.arrOne.length);
+      this.subscriptionOne.unsubscribe();
+    }
   }
 }
