7afe33ba49592d424b5d8f24cabbf64fd0cabf1c
mohit-rathi <mohit.rathi@bacancy.com>
2023-06-12 14:57:19 +0530

    Modify takeUntil logic and template

--- src/app/operator/take-until/take-until.component.ts
+++ src/app/operator/take-until/take-until.component.ts
@@ -1,27 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { Subject, interval, takeUntil } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, Subscription, interval, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-take-until',
   templateUrl: './take-until.component.html',
   styleUrls: ['./take-until.component.scss'],
 })
-export class TakeUntilComponent implements OnInit {
-  sub$ = new Subject();
+export class TakeUntilComponent implements OnDestroy {
+  // subject
+  private sub$ = new Subject();
 
-  takeUntilOneArr: number[] = [];
+  // Example - 01
+  private obs1$ = interval(1000);
+  private takeUntil1$ = this.obs1$.pipe(takeUntil(this.sub$));
+  private subscriptionOne!: Subscription;
+  public arrOne: number[] = [];
 
-  ngOnInit(): void {
-    const obs1$ = interval(1000);
-    const takeUntil1$ = obs1$.pipe(takeUntil(this.sub$));
+  ngOnDestroy(): void {
+    if (this.subscriptionOne) {
+      this.subscriptionOne.unsubscribe();
+    }
+  }
 
-    takeUntil1$.subscribe({
-      next: (data) => {
-        this.takeUntilOneArr.push(data);
+  // Example - 01
+  public subscribeOne(): void {
+    this.unsubscribeOne();
+    this.subscriptionOne = this.takeUntil1$.subscribe({
+      next: (res) => {
+        this.arrOne.push(res);
       },
     });
   }
 
+  // Example - 01
+  public unsubscribeOne(): void {
+    if (this.subscriptionOne) {
+      this.arrOne.splice(0, this.arrOne.length);
+      this.subscriptionOne.unsubscribe();
+    }
+  }
+
+  // Example - 01
   stop() {
     this.sub$.next(0);
   }
