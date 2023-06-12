0673f63e485682e0f7cc8a4481ba6cb46b9d22e7
mohit-rathi <mohit.rathi@bacancy.com>
2023-06-12 14:39:29 +0530

    Modify and add example for tap operator

--- src/app/operator/tap/tap.component.ts
+++ src/app/operator/tap/tap.component.ts
@@ -1,28 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-import { of, tap } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription, filter, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-tap',
   templateUrl: './tap.component.html',
   styleUrls: ['./tap.component.scss'],
 })
-export class TapComponent implements OnInit {
-  public tapArr: number[] = [];
-  public subArr: number[] = [];
+export class TapComponent implements OnDestroy {
+  // Example - 01
+  private obs1$ = of(1, 2, 3, 4, 5);
+  private tap1$ = this.obs1$.pipe(
+    tap((res) => {
+      this.arrOne.push(`From tap: ${res}`);
+    })
+  );
+  private subscriptionOne!: Subscription;
+  public arrOne: string[] = [];
 
-  ngOnInit(): void {
-    const obs1$ = of([1, 2, 3, 4, 5]);
+  // Example - 02
+  private obs2$ = of(1, 2, 3, 4, 5);
+  private tap2$ = this.obs2$.pipe(
+    tap((x) => {
+      if (x % 2 === 0) {
+        this.arrTwo.push(`From tap: ${x}`);
+      }
+    }),
+    filter((x) => x % 2 === 1)
+  );
+  private subscriptionTwo!: Subscription;
+  public arrTwo: string[] = [];
 
-    obs1$
-      .pipe(
-        tap((data) => {
-          this.tapArr = data;
-        })
-      )
-      .subscribe({
-        next: (data) => {
-          this.subArr = data;
-        },
-      });
+  ngOnDestroy(): void {
+    if (this.subscriptionOne) {
+      this.subscriptionOne.unsubscribe();
+    }
+    if (this.subscriptionTwo) {
+      this.subscriptionTwo.unsubscribe();
+    }
+  }
+
+  // Example - 01
+  public subscribeOne(): void {
+    this.unsubscribeOne();
+    this.subscriptionOne = this.tap1$.subscribe();
+  }
+
+  // Example - 01
+  public unsubscribeOne(): void {
+    if (this.subscriptionOne) {
+      this.arrOne.splice(0, this.arrOne.length);
+      this.subscriptionOne.unsubscribe();
+    }
+  }
+
+  // Example - 02
+  public subscribeTwo(): void {
+    this.unsubscribeTwo();
+    this.subscriptionTwo = this.tap2$.subscribe({
+      next: (res) => {
+        this.arrTwo.push(`From sub: ${res}`);
+      },
+    });
+  }
+
+  // Example - 02
+  public unsubscribeTwo(): void {
+    if (this.subscriptionTwo) {
+      this.arrTwo.splice(0, this.arrTwo.length);
+      this.subscriptionTwo.unsubscribe();
+    }
   }
 }
